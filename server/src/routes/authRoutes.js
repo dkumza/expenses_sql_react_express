1f83b2a4d26a762cee5640f4b5ce7871f7d9e32c
Darius <dariuskumza@gmail.com>
2024-01-04 22:09:17 +0200

    hash pasw added

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -13,11 +13,11 @@ authRouter.post(
    '/api/auth/reg',
    asyncHandler(async (req, res) => {
       const { email, password } = req.body;
-      console.log(req.body);
+      const hashPsw = bcrypt.hashSync(password, 10);
 
       const sql = 'INSERT INTO users (email, password) VALUES (?,?)';
 
-      const [rows] = await pool.execute(sql, [email, password]);
+      const [rows] = await pool.execute(sql, [email, hashPsw]);
       res.json(rows);
    })
 );

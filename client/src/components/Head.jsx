a05315634127927328db017a461015f4841e4486
Darius <dariuskumza@gmail.com>
2024-01-07 13:15:28 +0200

    log out

--- client/src/components/Head.jsx
+++ client/src/components/Head.jsx
@@ -1,7 +1,17 @@
+import { useContext } from 'react';
+import { ExpContext } from './ExpContext';
+
 export const Head = () => {
+   const { logged, setLogged } = useContext(ExpContext);
    return (
-      <div className=" pt-8 pb-2 text-center text-4xl uppercase md:w-[560px]">
-         expenses tracker
+      <div className=" pt-8 pb-2 text-center text-4xl uppercase md:w-[560px] ">
+         <div
+            onClick={() => setLogged(false)}
+            className=" text-right pb-4 hover:cursor-pointer hover:underline text-xs text-sky-600"
+         >
+            <i className="bi bi-door-open"></i> Log Out
+         </div>
+         <h1>expenses tracker</h1>
       </div>
    );
 };
